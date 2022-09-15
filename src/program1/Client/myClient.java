9f4fc094d037f59d4082890eade7d2fe316c59b9
Kuliakaiwikuamoo Akioka <66751553+Kakioka@users.noreply.github.com>
2022-09-15 10:17:24 -1000

    buff reader

--- src/program1/Client/myClient.java
+++ src/program1/Client/myClient.java
@@ -1,7 +1,14 @@
 package program1.Client;
+import java.io.*;
+import java.net.*;
 
 public class myClient {
     public static void main (String[] args){
+
+        String sentence;
+        String modiifiedSentence;
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
         System.out.println("Hello World!");
         System.out.println("Once More");
     }
