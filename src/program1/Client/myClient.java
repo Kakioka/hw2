c912ff0fc64d6ec73dea35d8e9bb4d43cf3b39ed
Kuliakaiwikuamoo Akioka <66751553+Kakioka@users.noreply.github.com>
2022-09-15 10:05:38 -1000

    Initial commit

---
+++ src/program1/Client/myClient.java
@@ -0,0 +1,8 @@
+package program1.Client;
+
+public class myClient {
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Once More");
+    }
+}
